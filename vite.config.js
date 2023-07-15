fbdc7d052cfcdbb8e99273b84e064f12637b535b
pnewsam <paul.newsam@gmail.com>
2023-07-15 11:49:47 -0700

    Deploy commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ export default {
   root: "./src",
   base: "./",
   build: {
+    rollupOptions: {
+      input: {
+        main: "./src/index.html",
+        blog: "./src/blog/index.html",
+      },
+    },
     outDir: "../docs",
   },
 };
